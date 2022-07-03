92c5bb4a7a6f25c7b398f2d953d43d876bba5362
richterjakob <richterjakob@users.noreply.github.com>
2022-07-03 20:51:46 +0000

    deploy: cfa9edb5ca3ff293c83183484f7d92c586d8db70

--- cpp/search/all_6.js
+++ cpp/search/all_6.js
@@ -1,10 +1,13 @@
 var searchData=
 [
   ['get_0',['get',['../class_m_o_d_e_l_1_1_time_dependent_parameter.html#a4dcbdfd359c34b8d4af725808705733e',1,'MODEL::TimeDependentParameter']]],
-  ['get_5fmodel_1',['get_model',['../class_i_o_1_1_config_reader.html#acbfe926ecbf754503071f8c62a64b3be',1,'IO::ConfigReader']]],
-  ['get_5fnum_5ftime_5fsteps_2',['get_num_time_steps',['../class_i_o_1_1_config_reader.html#a30a6649ef956d00d48eb14118d7f4bf4',1,'IO::ConfigReader']]],
-  ['get_5fnum_5ftriplets_3',['get_num_triplets',['../class_m_o_d_e_l_1_1_model.html#a0568bb6ffb0ee3e0ddba35ef9d1ce196',1,'MODEL::Model']]],
-  ['get_5ftime_5fstep_5fsize_4',['get_time_step_size',['../class_i_o_1_1_config_reader.html#ae63fc5536e55937512435528dcd262bc',1,'IO::ConfigReader']]],
-  ['global_5feqn_5fids_5',['global_eqn_ids',['../class_m_o_d_e_l_1_1_block.html#ad338703d131cb419b5d85af5b6527108',1,'MODEL::Block']]],
-  ['global_5fvar_5fids_6',['global_var_ids',['../class_m_o_d_e_l_1_1_block.html#a0dc20753b52a12c3e8f094f26755cc60',1,'MODEL::Block']]]
+  ['get_5fbool_5fsimulation_5fparameter_1',['get_bool_simulation_parameter',['../class_i_o_1_1_config_reader.html#a8a17af470183fa6e3734d26a985211f5',1,'IO::ConfigReader::get_bool_simulation_parameter(std::string key)'],['../class_i_o_1_1_config_reader.html#a259786ae8302cc0fd414317d4695ae40',1,'IO::ConfigReader::get_bool_simulation_parameter(std::string key, bool def)']]],
+  ['get_5fint_5fsimulation_5fparameter_2',['get_int_simulation_parameter',['../class_i_o_1_1_config_reader.html#a684add1b8843da67198e619852bea435',1,'IO::ConfigReader::get_int_simulation_parameter(std::string key)'],['../class_i_o_1_1_config_reader.html#a7e28be5d3dc3d36f15a231a70caa0832',1,'IO::ConfigReader::get_int_simulation_parameter(std::string key, int def)']]],
+  ['get_5fmodel_3',['get_model',['../class_i_o_1_1_config_reader.html#acbfe926ecbf754503071f8c62a64b3be',1,'IO::ConfigReader']]],
+  ['get_5fnum_5ftime_5fsteps_4',['get_num_time_steps',['../class_i_o_1_1_config_reader.html#a30a6649ef956d00d48eb14118d7f4bf4',1,'IO::ConfigReader']]],
+  ['get_5fnum_5ftriplets_5',['get_num_triplets',['../class_m_o_d_e_l_1_1_model.html#a0568bb6ffb0ee3e0ddba35ef9d1ce196',1,'MODEL::Model']]],
+  ['get_5fscalar_5fsimulation_5fparameter_6',['get_scalar_simulation_parameter',['../class_i_o_1_1_config_reader.html#abb736d8ea428c43494301da8422cd90f',1,'IO::ConfigReader::get_scalar_simulation_parameter(std::string key)'],['../class_i_o_1_1_config_reader.html#ab9b23fa0fec12cbce562d8b33354df32',1,'IO::ConfigReader::get_scalar_simulation_parameter(std::string key, T def)']]],
+  ['get_5ftime_5fstep_5fsize_7',['get_time_step_size',['../class_i_o_1_1_config_reader.html#ae63fc5536e55937512435528dcd262bc',1,'IO::ConfigReader']]],
+  ['global_5feqn_5fids_8',['global_eqn_ids',['../class_m_o_d_e_l_1_1_block.html#ad338703d131cb419b5d85af5b6527108',1,'MODEL::Block']]],
+  ['global_5fvar_5fids_9',['global_var_ids',['../class_m_o_d_e_l_1_1_block.html#a0dc20753b52a12c3e8f094f26755cc60',1,'MODEL::Block']]]
 ];

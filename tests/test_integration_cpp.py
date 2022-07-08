39564d7e5b30a90db68bb3f8c43cf3bb7da7cb2b
Jakob <jakob.richter@tum.de>
2022-07-07 20:11:27 -0700

    Adapt test values to higher cpp accuracy

--- tests/test_integration_cpp.py
+++ tests/test_integration_cpp.py
@@ -278,7 +278,7 @@ def test_pusatile_flow_cstenosis_steady_pressure(tmpdir):
     results = run_test_case_by_name("pusatileFlow_CStenosis_steadyPressure", tmpdir)
     assert np.isclose(
         get_result(results, "pressure", 0, 0, -439),
-        0.5937166987044968,
+        0.5933049197138334,
         rtol=1.0e-5,
     )  # inlet pressure
     assert np.isclose(
@@ -286,12 +286,12 @@ def test_pusatile_flow_cstenosis_steady_pressure(tmpdir):
     )  # outlet pressure
     assert np.isclose(
         get_result(results, "flow", 0, 0, -439),
-        0.7026499697830042,
+        0.7023611813029965,
         rtol=1.0e-5,
     )  # inlet flow
     assert np.isclose(
         get_result(results, "flow", 0, -1, -439),
-        0.7026499697830042,
+        0.7023611813029965,
         rtol=1.0e-5,
     )  # outlet flow
 

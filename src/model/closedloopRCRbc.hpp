21d144d0d6a9fc35f50dcf74b9c8dd2b4b3ccb8b
Karthik Menon <kmenon13@sh02-ln01.stanford.edu>
2022-09-20 17:04:14 -0700

    Added license text and fixed circuit diagrams

--- src/model/closedloopRCRbc.hpp
+++ src/model/closedloopRCRbc.hpp
@@ -1,3 +1,32 @@
+// Copyright (c) Stanford University, The Regents of the University of
+//               California, and others.
+//
+// All Rights Reserved.
+//
+// See Copyright-SimVascular.txt for additional details.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject
+// to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+// OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 /**
  * @file closedloopRCRbc.hpp
  * @brief MODEL::ClosedLoopRCRBC source file
@@ -19,13 +48,12 @@ namespace MODEL {
  * \begin{circuitikz} \draw
  * node[left] {$Q_{in}$} [-latex] (0,0) -- (0.8,0);
  * \draw (1,0) node[anchor=south]{$P_{in}$}
- * \draw (3,0) node[anchor=south]{$P_{C}$}
  * to [R, l=$R_p$, *-] (3,0)
  * to [R, l=$R_d$, *-*] (5,0)
  * node[anchor=south]{$P_{out}$}
- * node[left] {$Q_{out}$} [-latex] (5.2,0) -- (6.0,0);
  * (3,0) to [C, l=$C$, *-] (3,-1.5)
  * node[ground]{$P_{C}$};
+ * \draw node[left] {$Q_{out}$} [-latex] (5.2,0) -- (6.0,0);
  * \end{circuitikz}
  * \f]
  *

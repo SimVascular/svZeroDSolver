283f01d991ee10933353de16e303da64d528746f
Karthik Menon <kmenon13@sh03-ln04.stanford.edu>
2022-09-21 21:23:19 -0700

    Updated interface to reflect latest code svzerod structure

--- src/interface/interface.cpp
+++ src/interface/interface.cpp
@@ -61,9 +61,9 @@ SolverInterface::~SolverInterface()
 //            Callible interface functions              //
 //////////////////////////////////////////////////////////
 
-extern "C" void initialize(const char* input_file, const double time_step, int& problem_id, int& system_size);
+extern "C" void initialize(const char* input_file, const double external_time_step, int& problem_id, int& system_size);
 
-extern "C" void increment_time(const int problem_id, const double time, std::vector<double>& solution);
+extern "C" void increment_time(const int problem_id, const double external_time, std::vector<double>& solution);
 
 //------------
 // initialize
@@ -78,10 +78,10 @@ extern "C" void increment_time(const int problem_id, const double time, std::vec
 //
 void initialize(const char* input_file_arg, const double external_time_step, int& problem_id, int& system_size)
 {
-  std::cout << "========== svzero initialize ==========" << std::endl;
+  std::cout << "========== svZeroD initialize ==========" << std::endl;
   std::string input_file(input_file_arg);
   std::cout << "[initialize] input_file: " << input_file << std::endl;
-  std::string output_file = "svzerod.json";
+  std::string output_file = "svzerod.csv";
 
   auto interface = new SolverInterface(input_file);
   interface->external_time_step_ = external_time_step;
@@ -89,23 +89,34 @@ void initialize(const char* input_file_arg, const double external_time_step, int
   std::cout << "[initialize] problem_id: " << problem_id << std::endl;
 
   // Create configuration reader.
-  IO::ConfigReader<T> config(input_file);
+  //IO::ConfigReader<T> config(input_file);
+  IO::ConfigReader<T> reader;
+  reader.load(input_file);
 
   // Create a model.
-  auto model = config.get_model();
+  //auto model = config.get_model();
+  auto model = reader.model;
   interface->model_ = model; 
+  system_size = model->dofhandler.size();
   system_size = model->dofhandler.size();
   std::cout << "[initialize] System size: " << system_size << std::endl;
 
   // Get simulation parameters from the input JSON file.
   //
-  T time_step_size = config.get_time_step_size();
-  int num_time_steps = config.get_num_time_steps();
-  T absolute_tolerance = config.get_scalar_simulation_parameter("absolute_tolerance", 1e-8);
-  int max_nliter = config.get_int_simulation_parameter("maximum_nonlinear_iterations", 30);
-  int output_interval = config.get_int_simulation_parameter("output_interval", 1);
-  bool steady_initial = config.get_bool_simulation_parameter("steady_initial", true);
-  bool output_mean_only = config.get_bool_simulation_parameter("output_mean_only", false);
+  T time_step_size = reader.sim_time_step_size;
+  int num_time_steps = reader.sim_num_time_steps;
+  T absolute_tolerance = reader.sim_abs_tol;
+  int max_nliter = reader.sim_nliter;
+  int output_interval = reader.output_interval;
+  bool output_mean_only = reader.output_mean_only;
+
+//T time_step_size = config.get_time_step_size();
+//int num_time_steps = config.get_num_time_steps();
+//T absolute_tolerance = config.get_scalar_simulation_parameter("absolute_tolerance", 1e-8);
+//int max_nliter = config.get_int_simulation_parameter("maximum_nonlinear_iterations", 30);
+//int output_interval = config.get_int_simulation_parameter("output_interval", 1);
+//bool steady_initial = config.get_bool_simulation_parameter("steady_initial", true);
+//bool output_mean_only = config.get_bool_simulation_parameter("output_mean_only", false);
 
   std::cout << "[initialize] svzero parameters " << std::endl;
   std::cout << "[initialize]   num_time_steps: " << num_time_steps << std::endl;
@@ -118,18 +129,22 @@ void initialize(const char* input_file_arg, const double external_time_step, int
   interface->max_nliter_ = max_nliter;
   interface->absolute_tolerance_ = absolute_tolerance;
 
-  // Create initial state. 
-  ALGEBRA::State<T> state = ALGEBRA::State<T>::Zero(model->dofhandler.size());
+  // Create initial state.
+  ALGEBRA::State<T> state = reader.initial_state;
+ // ALGEBRA::State<T> state = ALGEBRA::State<T>::Zero(model->dofhandler.size());
 
   // Create steady initial state.
   //
-  if (steady_initial) {
+  //if (steady_initial) {
+  if (reader.sim_steady_initial) {
     std::cout << "[initialize] " << std::endl;
     std::cout << "[initialize] ----- Calculating steady initial condition ----- " << std::endl;
-    T time_step_size_steady = config.cardiac_cycle_period / 10.0;
+    //T time_step_size_steady = config.cardiac_cycle_period / 10.0;
+    T time_step_size_steady = reader.sim_cardiac_cycle_period / 10.0;
     std::cout << "[initialize] Create steady model ... " << std::endl;
 
-    auto model_steady = config.get_model();
+    //auto model_steady = config.get_model();
+    auto model_steady = reader.model;
     model_steady->to_steady();
 
     ALGEBRA::Integrator<T, S> integrator_steady(*model_steady, time_step_size_steady, 0.1, absolute_tolerance, max_nliter);
@@ -143,6 +158,7 @@ void initialize(const char* input_file_arg, const double external_time_step, int
   interface->time_step_ = 0;
   interface->save_interval_counter_ = 0;
   interface->output_interval_ = output_interval;
+  interface->system_size_ = system_size;
 
   std::cout << "[initialize] Done " << std::endl;
 }
@@ -151,25 +167,25 @@ void initialize(const char* input_file_arg, const double external_time_step, int
 // increment_time
 //----------------
 //
-void increment_time(const int problem_id, const double time, std::vector<double>& solution)
+void increment_time(const int problem_id, const double external_time, std::vector<double>& solution)
 {
   //std::cout << "[increment_time] " << std::endl;
-  //std::cout << "[increment_time] ========== svzero increment_time ==========" << std::endl;
+  //std::cout << "[increment_time] ========== svZeroD increment_time ==========" << std::endl;
   auto interface = SolverInterface::interface_list_[problem_id];
   auto model = interface->model_;
 
-  // [TODO] Use solver time step or svzero time step?
   auto time_step_size = interface->time_step_size_;
   auto absolute_tolerance = interface->absolute_tolerance_;
   auto max_nliter = interface->max_nliter_;
-  //std::cout << "[increment_time] time: " << time << std::endl;
+  //std::cout << "[increment_time] external_time: " << external_time << std::endl;
 
-  ALGEBRA::Integrator<T, S> integrator(*model, time, 0.1, absolute_tolerance, max_nliter);
+  //ALGEBRA::Integrator<T, S> integrator(*model, external_time, 0.1, absolute_tolerance, max_nliter);
+  ALGEBRA::Integrator<T, S> integrator(*model, time_step_size, 0.1, absolute_tolerance, max_nliter);
   auto state = interface->state_;
   interface->state_ = integrator.step(state, time, *model);
   interface->time_step_ += 1;
 
-  int state_size = state.y.size();
+  //int state_size = state.y.size();
   //std::cout << "[increment_time] state_size: " << state_size << std::endl;
   //std::cout << "[increment_time] solution size: " << solution.size() << std::endl;
 

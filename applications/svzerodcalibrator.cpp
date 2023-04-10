12a8b7c3929ee6a255ed026416b206a883386d50
Jakob Richter <103637279+richterjakob@users.noreply.github.com>
2023-04-10 21:49:32 +0200

    Add termination criteria

--- applications/svzerodcalibrator.cpp
+++ applications/svzerodcalibrator.cpp
@@ -3,14 +3,13 @@
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
 #include <fstream>
+#include <nlohmann/json.hpp>
 
 #include "helpers/debug.hpp"
 #include "helpers/endswith.hpp"
 #include "io/jsonhandler.hpp"
 #include "model/model.hpp"
 #include "optimize/levenbergmarquardtoptimizer.hpp"
-
-#include <nlohmann/json.hpp>
 
 // Setting scalar type to double
 typedef double T;
@@ -45,11 +44,13 @@ int main(int argc, char *argv[]) {
   std::vector<std::tuple<std::string, std::string>> outlet_connections;
 
   auto calibration_parameters = handler["calibration_parameters"];
-  bool calibrate_stenosis = calibration_parameters.get_bool("calibrate_stenosis_coefficient");
-  bool zero_capacitance = calibration_parameters.get_bool("set_capacitance_to_zero");
+  bool calibrate_stenosis =
+      calibration_parameters.get_bool("calibrate_stenosis_coefficient");
+  bool zero_capacitance =
+      calibration_parameters.get_bool("set_capacitance_to_zero");
 
   int num_params = 3;
-  if (calibrate_stenosis){
+  if (calibrate_stenosis) {
     num_params = 4;
   }
 
@@ -63,13 +64,10 @@ int main(int argc, char *argv[]) {
     std::string vessel_name = vessel_config.get_string("vessel_name");
     vessel_id_map.insert({vessel_config.get_int("vessel_id"), vessel_name});
     std::vector<int> param_ids;
-    for (size_t k = 0; k < num_params; k++)
-    {
-        param_ids.push_back(param_counter++);
+    for (size_t k = 0; k < num_params; k++) {
+      param_ids.push_back(param_counter++);
     }
-    model->add_block(MODEL::BlockType::BLOODVESSEL,
-                     param_ids,
-                     vessel_name);
+    model->add_block(MODEL::BlockType::BLOODVESSEL, param_ids, vessel_name);
     DEBUG_MSG("Created vessel " << vessel_name);
 
     // Read connected boundary conditions
@@ -124,12 +122,16 @@ int main(int argc, char *argv[]) {
 
   for (auto &connection : inlet_connections) {
     auto ele = model->get_block(std::get<1>(connection));
-    model->add_node({}, {ele}, static_cast<std::string>(std::get<0>(connection)) + ":" + ele->get_name());
+    model->add_node({}, {ele},
+                    static_cast<std::string>(std::get<0>(connection)) + ":" +
+                        ele->get_name());
   }
 
   for (auto &connection : outlet_connections) {
     auto ele = model->get_block(std::get<0>(connection));
-    model->add_node({ele}, {}, ele->get_name() + ":" + static_cast<std::string>(std::get<1>(connection)));
+    model->add_node({ele}, {},
+                    ele->get_name() + ":" +
+                        static_cast<std::string>(std::get<1>(connection)));
   }
 
   model->finalize();
@@ -148,19 +150,16 @@ int main(int argc, char *argv[]) {
   auto y_values = handler["y"];
   auto dy_values = handler["dy"];
   for (size_t i = 0; i < model->dofhandler.get_num_variables(); i++) {
-
     std::string var_name = model->dofhandler.variables[i];
     DEBUG_MSG("Reading observations for variable " << var_name);
     auto y_array = y_values.get_double_array(var_name);
     auto dy_array = dy_values.get_double_array(var_name);
     num_obs = y_array.size();
-    if (i==0)
-    {
+    if (i == 0) {
       y_all.resize(num_obs);
       dy_all.resize(num_obs);
     }
-    for (size_t j = 0; j < num_obs; j++)
-    {
+    for (size_t j = 0; j < num_obs; j++) {
       y_all[j].push_back(y_array[j]);
       dy_all[j].push_back(dy_array[j]);
     }
@@ -173,149 +172,137 @@ int main(int argc, char *argv[]) {
   Eigen::Matrix<T, Eigen::Dynamic, 1> alpha =
       Eigen::Matrix<T, Eigen::Dynamic, 1>::Zero(param_counter);
   DEBUG_MSG("Reading initial alpha");
-  for (auto &vessel_config : output_config["vessels"])
-  {
+  for (auto &vessel_config : output_config["vessels"]) {
     std::string vessel_name = vessel_config["vessel_name"];
     DEBUG_MSG("Reading initial alpha for " << vessel_name);
     auto block = model->get_block(vessel_name);
-    alpha[block->global_param_ids[0]] = vessel_config["zero_d_element_values"].value("R_poiseuille", 0.0);
-    alpha[block->global_param_ids[1]] = vessel_config["zero_d_element_values"].value("C", 0.0);
-    alpha[block->global_param_ids[2]] = vessel_config["zero_d_element_values"].value("L", 0.0);
-    if (num_params > 3)
-    {
-      alpha[block->global_param_ids[3]] = vessel_config["zero_d_element_values"].value("stenosis_coefficient", 0.0);
+    alpha[block->global_param_ids[0]] =
+        vessel_config["zero_d_element_values"].value("R_poiseuille", 0.0);
+    alpha[block->global_param_ids[1]] =
+        vessel_config["zero_d_element_values"].value("C", 0.0);
+    alpha[block->global_param_ids[2]] =
+        vessel_config["zero_d_element_values"].value("L", 0.0);
+    if (num_params > 3) {
+      alpha[block->global_param_ids[3]] =
+          vessel_config["zero_d_element_values"].value("stenosis_coefficient",
+                                                       0.0);
     }
   }
-  for (auto &junction_config : output_config["junctions"])
-  {
-
+  for (auto &junction_config : output_config["junctions"]) {
     std::string junction_name = junction_config["junction_name"];
     DEBUG_MSG("Reading initial alpha for " << junction_name);
     auto block = model->get_block(junction_name);
     int num_outlets = block->outlet_nodes.size();
 
-    if (num_outlets < 2)
-    {
-        continue;
+    if (num_outlets < 2) {
+      continue;
     }
 
-    for (size_t i = 0; i < num_outlets; i++)
-    {
-        alpha[block->global_param_ids[i]] = 0.0;
-        alpha[block->global_param_ids[i+num_outlets]] = 0.0;
-        alpha[block->global_param_ids[i+2*num_outlets]] = 0.0;
-        if (num_params > 3) {
-          alpha[block->global_param_ids[i+3*num_outlets]] = 0.0;
-        }
+    for (size_t i = 0; i < num_outlets; i++) {
+      alpha[block->global_param_ids[i]] = 0.0;
+      alpha[block->global_param_ids[i + num_outlets]] = 0.0;
+      alpha[block->global_param_ids[i + 2 * num_outlets]] = 0.0;
+      if (num_params > 3) {
+        alpha[block->global_param_ids[i + 3 * num_outlets]] = 0.0;
+      }
     }
-    if (junction_config["junction_type"] == "BloodVesselJunction")
-    {
-      auto resistance = junction_config["junction_values"]["R_poiseuille"].get<std::vector<double>>();
-      auto capacitance = junction_config["junction_values"]["C"].get<std::vector<double>>();
-      auto inductance = junction_config["junction_values"]["L"].get<std::vector<double>>();
-      auto stenosis_coeff = junction_config["junction_values"]["stenosis_coefficient"].get<std::vector<double>>();
-      for (size_t i = 0; i < num_outlets; i++)
-      {
-          alpha[block->global_param_ids[i]] = resistance[i];
-          alpha[block->global_param_ids[i+num_outlets]] = capacitance[i];
-          alpha[block->global_param_ids[i+2*num_outlets]] = inductance[i];
-          if (num_params > 3) {
-            alpha[block->global_param_ids[i+3*num_outlets]] = stenosis_coeff[i];
-          }
+    if (junction_config["junction_type"] == "BloodVesselJunction") {
+      auto resistance = junction_config["junction_values"]["R_poiseuille"]
+                            .get<std::vector<double>>();
+      auto capacitance =
+          junction_config["junction_values"]["C"].get<std::vector<double>>();
+      auto inductance =
+          junction_config["junction_values"]["L"].get<std::vector<double>>();
+      auto stenosis_coeff =
+          junction_config["junction_values"]["stenosis_coefficient"]
+              .get<std::vector<double>>();
+      for (size_t i = 0; i < num_outlets; i++) {
+        alpha[block->global_param_ids[i]] = resistance[i];
+        alpha[block->global_param_ids[i + num_outlets]] = capacitance[i];
+        alpha[block->global_param_ids[i + 2 * num_outlets]] = inductance[i];
+        if (num_params > 3) {
+          alpha[block->global_param_ids[i + 3 * num_outlets]] =
+              stenosis_coeff[i];
+        }
       }
     }
   }
-
 
   // =====================================
   // Levenberg Marquardt
   // =====================================
   DEBUG_MSG("Start optimization");
-  auto lm_alg = OPT::LevenbergMarquardtOptimizer(model.get(), num_obs, param_counter, 1.0);
-  
+  auto lm_alg = OPT::LevenbergMarquardtOptimizer(
+      model.get(), num_obs, param_counter, 1.0, 1.0e-5, 1.0e-10, 100);
+
   alpha = lm_alg.run(alpha, y_all, dy_all);
 
   // =====================================
   // Write output
   // =====================================
-  for (auto &vessel_config : output_config["vessels"])
-  {
+  for (auto &vessel_config : output_config["vessels"]) {
     std::string vessel_name = vessel_config["vessel_name"];
     auto block = model->get_block(vessel_name);
     T stenosis_coeff = 0.0;
-    if (num_params > 3)
-    {
+    if (num_params > 3) {
       stenosis_coeff = alpha[block->global_param_ids[3]];
     }
     T c_value = 0.0;
-    if (!zero_capacitance)
-    {
+    if (!zero_capacitance) {
       c_value = alpha[block->global_param_ids[1]];
     }
     vessel_config["zero_d_element_values"] = {
         {"R_poiseuille", alpha[block->global_param_ids[0]]},
         {"C", std::max(c_value, 0.0)},
         {"L", std::max(alpha[block->global_param_ids[2]], 0.0)},
-        {"stenosis_coefficient", stenosis_coeff}
-    };
+        {"stenosis_coefficient", stenosis_coeff}};
   }
 
-  for (auto &junction_config : output_config["junctions"])
-  {
-
+  for (auto &junction_config : output_config["junctions"]) {
     std::string junction_name = junction_config["junction_name"];
     auto block = model->get_block(junction_name);
     int num_outlets = block->outlet_nodes.size();
 
-    if (num_outlets < 2)
-    {
-        continue;
+    if (num_outlets < 2) {
+      continue;
     }
 
     std::vector<T> r_values;
-    for (size_t i = 0; i < num_outlets; i++)
-    {
-        r_values.push_back(alpha[block->global_param_ids[i]]);
+    for (size_t i = 0; i < num_outlets; i++) {
+      r_values.push_back(alpha[block->global_param_ids[i]]);
     }
     std::vector<T> c_values;
-    if (zero_capacitance)
-    {
-      for (size_t i = 0; i < num_outlets; i++)
-      {
-          c_values.push_back(0.0);
+    if (zero_capacitance) {
+      for (size_t i = 0; i < num_outlets; i++) {
+        c_values.push_back(0.0);
       }
-    }
-    else {
-      for (size_t i = 0; i < num_outlets; i++)
-      {
-          c_values.push_back(std::max(alpha[block->global_param_ids[i+num_outlets]], 0.0));
+    } else {
+      for (size_t i = 0; i < num_outlets; i++) {
+        c_values.push_back(
+            std::max(alpha[block->global_param_ids[i + num_outlets]], 0.0));
       }
     }
     std::vector<T> l_values;
-    for (size_t i = 0; i < num_outlets; i++)
-    {
-        l_values.push_back(std::max(alpha[block->global_param_ids[i+2*num_outlets]],0.0));
+    for (size_t i = 0; i < num_outlets; i++) {
+      l_values.push_back(
+          std::max(alpha[block->global_param_ids[i + 2 * num_outlets]], 0.0));
     }
     std::vector<T> ste_values;
-    if (num_params > 3)
-    {
-        for (size_t i = 0; i < num_outlets; i++){
-          ste_values.push_back(alpha[block->global_param_ids[i+3*num_outlets]]);
-        }
-    }
-    else {
-      for (size_t i = 0; i < num_outlets; i++)
-      {
-          ste_values.push_back(0.0);
+    if (num_params > 3) {
+      for (size_t i = 0; i < num_outlets; i++) {
+        ste_values.push_back(
+            alpha[block->global_param_ids[i + 3 * num_outlets]]);
+      }
+    } else {
+      for (size_t i = 0; i < num_outlets; i++) {
+        ste_values.push_back(0.0);
       }
     }
     junction_config["junction_type"] = "BloodVesselJunction";
-    junction_config["junction_values"] = {
-        {"R_poiseuille", r_values},
-        {"C", c_values},
-        {"L", l_values},
-        {"stenosis_coefficient", ste_values}
-    };
+    junction_config["junction_values"] = {{"R_poiseuille", r_values},
+                                          {"C", c_values},
+                                          {"L", l_values},
+                                          {"stenosis_coefficient", ste_values}};
   }
 
   output_config.erase("y");
--- src/optimize/levenbergmarquardtoptimizer.hpp
+++ src/optimize/levenbergmarquardtoptimizer.hpp
@@ -42,6 +42,62 @@ namespace OPT {
 /**
  * @brief Levenberg-Marquardt optimization class
  *
+ * The 0D residual (assuming no time-dependency in parameters) is
+ *
+ * \f[
+ * \boldsymbol{r}(\boldsymbol{\alpha}, \boldsymbol{y}, \boldsymbol{\dot{y}}) =
+ * \boldsymbol{E}(\boldsymbol{\alpha}, \boldsymbol{y}) \cdot
+ * \dot{\boldsymbol{y}}+\boldsymbol{F}(\boldsymbol{\alpha}, \boldsymbol{y})
+ * \cdot \boldsymbol{y}+\boldsymbol{c}(\boldsymbol{\alpha}, \boldsymbol{y}) \f]
+ *
+ * with solution vector \f$\boldsymbol{y} \in \mathbb{R}^{N}\f$ (flow and
+ * pressure at nodes), LPN parameters \f$\boldsymbol{\alpha} \in
+ * \mathbb{R}^{P}\f$, system matrices \f$\boldsymbol{E},\boldsymbol{F} \in
+ * \mathbb{R}^{NxN}\f$, and system vector \f$\boldsymbol{c} \in
+ * \mathbb{R}^{N}\f$.
+ *
+ * The least squares problem can be formulated as
+ *
+ * \f[
+ * \min _\alpha S, \quad \mathrm { with } \quad S=\sum_i^D
+ * r_i^2\left(\boldsymbol{\alpha}, y_i, \dot{y}_i\right) \f]
+ *
+ * with given solution vectors \f$\boldsymbol{y}\f$, \f$\boldsymbol{\dot{y}}\f$
+ * at all datapoints \f$D\f$. The parameter vector is iteratively improved
+ * according to
+ *
+ * \f[
+ * \boldsymbol{\alpha}^{i+1}=\boldsymbol{\alpha}^{i}+\Delta
+ * \boldsymbol{\alpha}^{i+1} \f]
+ * 
+ * wherein the increment \f$\Delta \boldsymbol{\alpha}^{i+1} \f$ is determined 
+ * by solving the following system:
+ *
+ * \f[
+ * \left[\mathbf{J}^{\mathrm{T}} \mathbf{J}+\lambda
+ * \operatorname{diag}\left(\mathbf{J}^{\mathrm{T}} \mathbf{J}\right)\right]^{i}
+ * \cdot \Delta \boldsymbol{\alpha}^{i+1}=-\left[\mathbf{J}^{\mathrm{T}}
+ * \mathbf{r}\right]^{i}, \quad \lambda^{i}=\lambda^{i-1}
+ * \cdot\left\|\left[\mathbf{J}^{\mathrm{T}} \mathbf{r}\right]^{i}\right\|_2
+ * /\left\|\left[\mathbf{J}^{\mathrm{T}} \mathbf{r}\right]^{i-1}\right\|_2. \f]
+ * 
+ * The algorithm terminates when the following tolerance thresholds are reached
+ *
+ * \f[
+ * \left\|\left[\mathbf{J}^{\mathrm{T}}
+ * \mathbf{r}\right]^{\mathrm{i}}\right\|_2<\operatorname{tol}_{\text {grad
+ * }}^\alpha \text { and }\left\|\Delta
+ * \boldsymbol{\alpha}^{\mathrm{i}+1}\right\|_2<\mathrm{tol}_{\text {inc
+ * }}^\alpha, \f]
+ * 
+ * The Jacobian is derived from the residual as
+ *
+ * \f[
+ * J = \frac{\partial \boldsymbol{r}}{\partial \boldsymbol{\alpha}} =
+ * \frac{\partial \mathbf{E}}{\partial \boldsymbol{\alpha}} \cdot
+ * \dot{\mathbf{y}}+\frac{\partial \mathbf{F}}{\partial \boldsymbol{\alpha}}
+ * \cdot \mathbf{y}+\frac{\partial \mathbf{c}}{\partial \boldsymbol{\alpha}} \f]
+ *
  *
  * @tparam T Scalar type (e.g. `float`, `double`)
  */
@@ -54,10 +110,19 @@ class LevenbergMarquardtOptimizer {
    * @param model The 0D model
    * @param num_obs Number of observations in optimization
    * @param num_params Number of parameters in optimization
-   * @param lambda Damping factor
+   * @param lambda0 Initial damping factor
+   * @param tol_grad Gradient tolerance
+   * @param tol_inc Parameter increment tolerance
+   * @param max_iter Maximum iterations
    */
   LevenbergMarquardtOptimizer(MODEL::Model<T>* model, int num_obs,
-                              int num_params, T lambda);
+                              int num_params, T lambda0, T tol_grad, T tol_inc,
+                              int max_iter);
+
+  /**
+   * @brief Destroy the LevenbergMarquardtOptimizer object
+   *
+   */
   ~LevenbergMarquardtOptimizer();
 
   /**
@@ -88,6 +153,10 @@ class LevenbergMarquardtOptimizer {
   int num_vars;
   int num_dpoints;
 
+  T tol_grad;
+  T tol_inc;
+  int max_iter;
+
   void update_gradient(Eigen::Matrix<T, Eigen::Dynamic, 1>& alpha,
                        std::vector<std::vector<T>>& y_obs,
                        std::vector<std::vector<T>>& dy_obs);
@@ -97,14 +166,18 @@ class LevenbergMarquardtOptimizer {
 
 template <typename T>
 LevenbergMarquardtOptimizer<T>::LevenbergMarquardtOptimizer(
-    MODEL::Model<T>* model, int num_obs, int num_params, T lambda) {
+    MODEL::Model<T>* model, int num_obs, int num_params, T lambda0, T tol_grad,
+    T tol_inc, int max_iter) {
   this->model = model;
   this->num_obs = num_obs;
   this->num_params = num_params;
   this->num_eqns = model->dofhandler.get_num_equations();
   this->num_vars = model->dofhandler.get_num_variables();
   this->num_dpoints = this->num_obs * this->num_eqns;
-  this->lambda = lambda;
+  this->lambda = lambda0;
+  this->tol_grad = tol_grad;
+  this->tol_inc = tol_inc;
+  this->max_iter = max_iter;
   jacobian = Eigen::SparseMatrix<T>(num_dpoints, num_params);
   residual = Eigen::Matrix<T, Eigen::Dynamic, 1>::Zero(num_dpoints);
   mat =
@@ -119,17 +192,25 @@ template <typename T>
 Eigen::Matrix<T, Eigen::Dynamic, 1> LevenbergMarquardtOptimizer<T>::run(
     Eigen::Matrix<T, Eigen::Dynamic, 1> alpha,
     std::vector<std::vector<T>>& y_obs, std::vector<std::vector<T>>& dy_obs) {
-  for (size_t nliter = 0; nliter < 100; nliter++) {
+  for (size_t i = 0; i < max_iter; i++) {
     update_gradient(alpha, y_obs, dy_obs);
-    if (nliter == 0) {
+    if (i == 0) {
       update_delta(true);
     } else {
       update_delta(false);
     }
     alpha -= delta;
-    std::cout << std::setprecision(12) << "Iteration " << nliter + 1
-              << " | lambda: " << lambda << " | norm inc: " << delta.norm()
-              << " | norm grad: " << vec.norm() << std::endl;
+    T norm_grad = vec.norm();
+    T norm_inc = delta.norm();
+    std::cout << std::setprecision(12) << "Iteration " << i + 1
+              << " | lambda: " << lambda << " | norm inc: " << norm_inc
+              << " | norm grad: " << norm_grad << std::endl;
+    if ((norm_grad < tol_grad) && (norm_inc < tol_inc)) {
+      break;
+    }
+    if (i >= max_iter - 1) {
+      throw std::runtime_error("Maximum number of iterations reached");
+    }
   }
   return alpha;
 }

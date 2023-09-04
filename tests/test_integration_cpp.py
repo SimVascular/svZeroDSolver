f4c44b2be6d2b81cf0e8f2372ee612c89a999a9a
Martin R. Pfaller <pfaller@stanford.edu>
2023-09-04 13:02:30 -0700

    test io mean derivative

--- tests/test_integration_cpp.py
+++ tests/test_integration_cpp.py
@@ -414,6 +414,28 @@ def test_pulsatile_flow_r_rcr_derivative():
         np.isclose(ref, res, atol=tol[f[0]])
 
 
+def test_pulsatile_flow_r_rcr_mean_derivative():
+    # time-dependent results
+    res_time = run_test_case_by_name("pulsatileFlow_R_RCR", output_variable_based=True)
+
+    # time-averaged results
+    res_mean = run_test_case_by_name("pulsatileFlow_R_RCR_mean_derivative", output_variable_based=True)
+
+    # time step
+    dt = res_time["time"][1] - res_time["time"][0]
+
+    # fields to test
+    fields = ["pressure_in", "pressure_out", "flow_in", "flow_out"]
+
+    # use coarse absolute tolerances for gradient calculation
+    # we're comparing gradients from gen-alpha in svZeroDPlus with central differences in np.gradient
+    tol = {"f": 0.01, "p": 20.0}
+    for f in fields:
+        ref = np.mean(np.gradient(res_time[f], dt))
+        res = res_mean["d_" + f]
+        np.isclose(ref, res, atol=tol[f[0]])
+
+
 def test_pulsatile_flow_r_coronary():
     results = run_test_case_by_name("pulsatileFlow_R_coronary")
     assert np.isclose(

e1c9184ac79b2184fd7f6c61fd3e0a170e5a2eb8
Martin R. Pfaller <pfaller@stanford.edu>
2023-09-20 19:07:37 -0700

    add tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,8 +1,11 @@
-import os
 import numpy as np
 
-
 from .utils import run_test_case_by_name, RTOL_FLOW, RTOL_PRES
+
+# use coarse absolute tolerances for gradient calculation
+# we're comparing gradients from gen-alpha in svZeroDPlus with central differences in np.gradient
+ATOL = {"f": 0.6, "p": 800.0}
+ATOL_MEAN = {"f": 0.01, "p": 20.0}
 
 def test_pulsatile_flow_r_rcr_mean():
     # time-dependent results
@@ -50,14 +53,29 @@ def test_pulsatile_flow_r_rcr_derivative():
 
     # fields to test
     fields = ["pressure_in", "pressure_out", "flow_in", "flow_out"]
-
-    # use coarse absolute tolerances for gradient calculation
-    # we're comparing gradients from gen-alpha in svZeroDPlus with central differences in np.gradient
-    tol = {"f": 0.6, "p": 800.0}
     for f in fields:
         ref = np.gradient(results[f], dt)
         res = results["d_" + f]
-        assert np.all(np.isclose(ref, res, atol=tol[f[0]]))
+        assert np.all(np.isclose(ref, res, atol=ATOL[f[0]]))
+
+
+def test_pulsatile_flow_r_rcr_derivative_variable():
+    results = run_test_case_by_name(
+        "pulsatileFlow_R_RCR_derivative_variable", output_variable_based=True
+    )
+
+    # time step
+    dt = results["time"][1] - results["time"][0]
+
+    # fields and segments to test
+    fields = ["flow", "pressure"]
+    segments = ["INFLOW:branch0_seg0", "branch0_seg0:OUT"]
+    for f in fields:
+        for v in segments:
+            ids = results["name"] == f + ":" + v
+            ref = np.gradient(results.y[ids], dt)
+            res = results.ydot[ids]
+            assert np.all(np.isclose(ref, res, atol=ATOL[f[0]]))
 
 
 def test_pulsatile_flow_r_rcr_mean_derivative():
@@ -74,11 +92,29 @@ def test_pulsatile_flow_r_rcr_mean_derivative():
 
     # fields to test
     fields = ["pressure_in", "pressure_out", "flow_in", "flow_out"]
-
-    # use coarse absolute tolerances for gradient calculation
-    # we're comparing gradients from gen-alpha in svZeroDPlus with central differences in np.gradient
-    tol = {"f": 0.01, "p": 20.0}
     for f in fields:
         ref = np.mean(np.gradient(res_time[f], dt))
         res = res_mean["d_" + f]
-        assert np.isclose(ref, res, atol=tol[f[0]])
+        assert np.isclose(ref, res, atol=ATOL_MEAN[f[0]])
+
+
+def test_pulsatile_flow_r_rcr_mean_derivative_variable():
+    # time-dependent results
+    res_time = run_test_case_by_name("pulsatileFlow_R_RCR_variable", output_variable_based=True)
+
+    # time-averaged results
+    res_mean = run_test_case_by_name(
+        "pulsatileFlow_R_RCR_mean_derivative_variable", output_variable_based=True
+    )
+
+    # time step
+    dt = res_time["time"][1] - res_time["time"][0]
+
+    # fields and segments to test
+    fields = ["flow", "pressure"]
+    segments = {"in": "INFLOW:branch0_seg0", "out": "branch0_seg0:OUT"}
+    for f in fields:
+        for k, v in segments.items():
+            rt = np.mean(np.gradient(res_time[f + "_" + k], dt))
+            rm = res_mean["ydot"][res_mean["name"] == f + ":" + v]
+            assert np.isclose(rt, rm, atol=ATOL_MEAN[f[0]])

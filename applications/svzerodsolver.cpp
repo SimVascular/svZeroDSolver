d870b92951f1addfa09fe59e8bb7b70bc41f0075
Dave Parker <davep@stanford.edu>
2023-10-31 17:44:27 -0700

    Add some error checking to the apps.

--- applications/svzerodsolver.cpp
+++ applications/svzerodsolver.cpp
@@ -56,38 +56,56 @@ int main(int argc, char* argv[]) {
   DEBUG_MSG("Starting svZeroDSolver");
 
   // Get input and output file name
-  if (argc < 2 && argc > 3) {
-    std::runtime_error(
-        "Usage: svzerodsolver path/to/config.json "
-        "(optional:path/to/output.csv)");
+  if (argc < 2 || argc > 3) {
+    std::cout << "Usage: svzerodsolver path/to/config.json [path/to/output.csv]" << std::endl;;
+    return 1;
   }
 
-  std::string input_file = argv[1];
-  std::string output_file;
+  std::string input_file_name = argv[1];
+  std::string output_file_name;
 
   if (argc == 3) {
-    output_file = argv[2];
+    output_file_name = argv[2];
 
   } else {
     // If output file is not provided, default is <path to .json>+"output.csv"
+    std::size_t end_of_path = input_file_name.rfind("/");
 
-    std::size_t end_of_path = input_file.rfind("/");
     if (end_of_path == std::string::npos) {
-      end_of_path = input_file.rfind("\\");  // For Windows paths (?)
+      end_of_path = input_file_name.rfind("\\");  // For Windows paths (?)
+
       if (end_of_path == std::string::npos) {
-        std::runtime_error("Could not find path to .json file.");
+        std::cout << "[svzerodsolver] Error: Could not create a default output file." << std::endl;
+        return 1;
       }
     }
 
-    output_file = input_file.substr(0, end_of_path) + "output.csv";
+    output_file_name = input_file_name.substr(0, end_of_path) + "output.csv";
+    std::cout << "[svzerodsolver] Output will be written to '" << output_file_name << "'." << std::endl;;
   }
 
-  std::ifstream ifs(input_file);
-  const auto& config = nlohmann::json::parse(ifs);
+  std::ifstream input_file(input_file_name);
+
+  if (!input_file.is_open()) {
+    std::cerr << "[svzerodsolver] Error: The input file '" << input_file_name << "' cannot be opened." << std::endl;
+    return 1;
+  }
+
+  nlohmann::json config;
+
+  try { 
+    config = nlohmann::json::parse(input_file);
+
+  } catch (const nlohmann::json::parse_error& e) {
+    std::cout << "[svzerodsolver] Error: Parsing the input file '" << input_file_name << "' has failed." << std::endl;
+    std::cout << "[svzerodsolver] Details of the parsing error: " << std::endl;
+    std::cout << e.what() << std::endl;
+    return 1;
+  }
 
   auto solver = Solver(config);
   solver.run();
-  solver.write_result_to_csv(output_file);
+  solver.write_result_to_csv(output_file_name);
 
   return 0;
 }

7c517859632464dc051868642768226180e15d16
Karthik Menon <kmenon13@sh02-ln02.stanford.edu>
2023-11-27 06:48:52 -0800

    clang format problem

--- src/model/Model.cpp
+++ src/model/Model.cpp
@@ -28,8 +28,8 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include "Model.h"
 #include "Node.h"
+#include "Model.h"
 
 Model::Model() {}
 

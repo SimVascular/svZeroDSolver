d03188e1d5113b89dc4564610da4c0569738adcd
mrp089 <mrp089@users.noreply.github.com>
2024-01-18 16:10:30 +0000

    deploy: 208c09426b115fd88b7cb5e496d72258707b7b58

--- search/functions_d.js
+++ search/functions_d.js
@@ -1,9 +1,10 @@
 var searchData=
 [
-  ['register_5fequation_0',['register_equation',['../class_d_o_f_handler.html#ac8a9df3b1ecafd8996eb48f1bacb7b00',1,'DOFHandler']]],
-  ['register_5fvariable_1',['register_variable',['../class_d_o_f_handler.html#a0bd43f0560bf0f8f9880555eed418ed9',1,'DOFHandler']]],
-  ['reserve_2',['reserve',['../class_sparse_system.html#a175040c26141bbe53e98bfd0cf3e4898',1,'SparseSystem']]],
-  ['resistancebc_3',['ResistanceBC',['../class_resistance_b_c.html#a30d3b325f9a07f7751c866febaa177f8',1,'ResistanceBC']]],
-  ['resistivejunction_4',['ResistiveJunction',['../class_resistive_junction.html#a93315135b8f0c0bf7c868d46a5c038ac',1,'ResistiveJunction']]],
-  ['run_5',['run',['../class_levenberg_marquardt_optimizer.html#af61872e32f3638b60631ad3fc834ab3f',1,'LevenbergMarquardtOptimizer::run()'],['../class_solver.html#a1f31e2d38dacd7e828c1bf428eb2ed96',1,'Solver::run()']]]
+  ['read_5fblock_5fparams_0',['read_block_params',['../class_solver.html#a0c0efb666ac941cf0d49c666b6356483',1,'Solver']]],
+  ['register_5fequation_1',['register_equation',['../class_d_o_f_handler.html#ac8a9df3b1ecafd8996eb48f1bacb7b00',1,'DOFHandler']]],
+  ['register_5fvariable_2',['register_variable',['../class_d_o_f_handler.html#a0bd43f0560bf0f8f9880555eed418ed9',1,'DOFHandler']]],
+  ['reserve_3',['reserve',['../class_sparse_system.html#a175040c26141bbe53e98bfd0cf3e4898',1,'SparseSystem']]],
+  ['resistancebc_4',['ResistanceBC',['../class_resistance_b_c.html#a30d3b325f9a07f7751c866febaa177f8',1,'ResistanceBC']]],
+  ['resistivejunction_5',['ResistiveJunction',['../class_resistive_junction.html#a93315135b8f0c0bf7c868d46a5c038ac',1,'ResistiveJunction']]],
+  ['run_6',['run',['../class_levenberg_marquardt_optimizer.html#af61872e32f3638b60631ad3fc834ab3f',1,'LevenbergMarquardtOptimizer::run()'],['../class_solver.html#a1f31e2d38dacd7e828c1bf428eb2ed96',1,'Solver::run()']]]
 ];

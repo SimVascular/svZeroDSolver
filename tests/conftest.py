772c58b9c40dc8a5ab97b3432127dcad798371a5
ncdorn <ndorn@stanford.edu>
2024-04-02 16:34:01 -0700

    slowly fixing test comparison issue

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ from tempfile import TemporaryDirectory
 
 import numpy as np
 import pytest
+from .utils import execute_pysvzerod
 
 
 def pytest_addoption(parser):
@@ -16,3 +17,22 @@ def pytest_addoption(parser):
 def pytest_configure(config):
     """Workaround because store_true doesn't work"""
     pytest.coverage = config.option.coverage == True
+
+
+def run_with_reference(
+        ref,
+        test_config
+        ):
+
+    RTOL_PRES = 1.0e-7
+    RTOL_FLOW = 1.0e-8
+    
+    res, config = execute_pysvzerod(test_config, "solver")
+
+    for field in ["pressure_in", "pressure_out", "flow_in", "flow_out"]:
+        if "pressure" in field:
+            assert np.isclose(res[field].to_numpy(), ref[field].to_numpy(), rtol=RTOL_PRES)
+        elif "flow" in field:
+            assert np.isclose(res[field].to_numpy(), ref[field].to_numpy(), rtol=RTOL_FLOW)
+
+    print('test passed!')

f6161ac664ff610a257ba7bdfb287ca00638ea8e
emilinmathew <emilinm@stanford.edu>
2024-08-12 15:54:52 -0700

    changed server name

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
-    baseUrl: 'http://localhost:8900',
+    baseUrl: 'http://localhost:8901',
     "supportFile": false,
     specPattern: 'tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },

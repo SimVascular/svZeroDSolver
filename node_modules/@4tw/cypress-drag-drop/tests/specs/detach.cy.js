f548a96f9be5d423c6fa0fb21deac5e24c316446
emilinmathew <emilinm@stanford.edu>
2024-08-13 12:07:16 -0700

    added pytest to requirements.txt

--- node_modules/@4tw/cypress-drag-drop/tests/specs/detach.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Drag drop', () => {
-  it('should not throw error when element is detached on drop', () => {
-    cy.visit('/')
-
-    cy.setExample('Detach')
-
-    cy.findByTestId('item-1').drag('[data-testid="right"]')
-  })
-})

263547ca2a5a430dbfad5bdc8347d67f25e07b72
emilinmathew <emilinm@stanford.edu>
2024-08-15 10:31:21 -0700

    Changed base url

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
-    baseUrl: 'http://localhost:8901',
+    baseUrl: 'http://localhost:8902',
     "supportFile": false,
     specPattern: 'tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },

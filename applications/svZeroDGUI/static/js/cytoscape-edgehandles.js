2940649be89bb3552c2ec3eba69d197d61194ae3
menon-karthik <karthikmenon.13@gmail.com>
2025-05-18 13:58:01 -0400

    updated license in every file

--- applications/svZeroDGUI/static/js/cytoscape-edgehandles.js
+++ applications/svZeroDGUI/static/js/cytoscape-edgehandles.js
@@ -1,3 +1,5 @@
+// SPDX-FileCopyrightText: Copyright (c) Stanford University, The Regents of the University of California, and others.
+// SPDX-License-Identifier: BSD-3-Clause
 /**
  * cytoscape-edgehandles.js
  *

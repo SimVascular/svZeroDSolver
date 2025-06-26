f3dc90534fc92c2a65e8e4d9c4ab12670cb2884c
rjrios915 <rjrios@stanford.edu>
2025-06-26 10:10:48 -0700

    Manually Revise ClangFormat

--- src/solve/SimulationParameters.cpp
+++ src/solve/SimulationParameters.cpp
@@ -421,8 +421,8 @@ void create_external_coupling(
           (connected_type == "BloodVesselA")) {
         connections.push_back({connected_block, coupling_name});
       }  // connected_type == "ClosedLoopRCR"
-    }    // coupling_loc
-  }      // for (size_t i = 0; i < coupling_configs.length(); i++)
+    }  // coupling_loc
+  }  // for (size_t i = 0; i < coupling_configs.length(); i++)
 }
 
 void create_junctions(
